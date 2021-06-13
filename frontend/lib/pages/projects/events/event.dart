40388f5c10c481c79749698ed2d38a6c53c173db
Dragonite24 <qwez-9@mail.ru>
2021-06-13 22:14:28 +0300

    event info

--- frontend/lib/pages/projects/events/event.dart
+++ frontend/lib/pages/projects/events/event.dart
@@ -33,7 +33,7 @@ class _EventCardState extends State<EventCard> {
           return UndergroundNoConnection();
         } else if (snapshot.hasData) {
           event = snapshot.data;
-          return eventInformation(index);
+          return eventInformation();
         } else {
           return Center(
             child: CircularProgressIndicator(),
@@ -43,7 +43,7 @@ class _EventCardState extends State<EventCard> {
     );
   }
 
-  Widget eventInformation(index) => UndergroundScaffold(
+  Widget eventInformation() => UndergroundScaffold(
         body: Container(
           child: Column(
             children: [
@@ -75,6 +75,14 @@ class _EventCardState extends State<EventCard> {
                     bottom: 15,
                     right: 15,
                     child: Container(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: <Color>[
+                            Color(0xFF16FF00),
+                            Color(0xFF4CFFC9),
+                          ],
+                        ),
+                      ),
                       height: 46,
                       child: FloatingActionButton(
                           backgroundColor: green,
@@ -90,49 +98,96 @@ class _EventCardState extends State<EventCard> {
               ),
               Container(
                 height: 500,
+                width: MediaQuery.of(context).size.width * 0.9,
                 color: Colors.white,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
+                    SizedBox(height: 15),
                     Text(
-                      'Колесников Семен',
+                      event.name,
                       overflow: TextOverflow.ellipsis,
                       style: TextStyle(
                               fontStyle: FontStyle.normal,
                               fontWeight: FontWeight.w600,
-                              fontSize: 20,
+                              fontSize: 24,
                               color: Color(0xFF333333))
-                          .copyWith(fontFamily: Fonts().light),
+                          .copyWith(fontFamily: Fonts().medium),
                     ),
+                    SizedBox(height: 5),
                     Text(
-                      'Россия, Ярославль',
+                      event.fullDesc,
+                      overflow: TextOverflow.ellipsis,
                       style: TextStyle(
                               fontStyle: FontStyle.normal,
                               fontWeight: FontWeight.w600,
-                              fontSize: 12,
+                              fontSize: 16,
                               color: Color(0xFF333333))
-                          .copyWith(fontFamily: Fonts().light),
+                          .copyWith(fontFamily: Fonts().thin),
                     ),
-                    SizedBox(height: MediaQuery.of(context).size.height * 0.01),
-                    RichText(
-                        textAlign: TextAlign.center,
-                        text: TextSpan(
-                            style: TextStyle(
+                    SizedBox(height: 50),
+                    Text(
+                      event.date.toString(),
+                      style: TextStyle(
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                        color: Color(0xFF333333),
+                      ).copyWith(fontFamily: Fonts().light),
+                    ),
+                    Text(
+                      event.shortDesc.toString(),
+                      style: TextStyle(
+                              fontStyle: FontStyle.normal,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16,
+                              color: Color(0xFF333333))
+                          .copyWith(fontFamily: Fonts().thin),
+                    ),
+                    SizedBox(height: 15),
+                    Text(
+                      'Контакты',
+                      style: TextStyle(
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                        color: Color(0xFF333333),
+                      ).copyWith(fontFamily: Fonts().light),
+                    ),
+                    Text(event.contactEmail,
+                        style: TextStyle(
                                 fontStyle: FontStyle.normal,
-                                fontWeight: FontWeight.w500,
-                                fontSize: 12,
-                                color: Color(0xFF333333)),
-                            children: <TextSpan>[
-                              TextSpan(
-                                  text: '+7(905)630-22-80 qwez-9@mail.ru',
-                                  style: TextStyle(
-                                      fontStyle: FontStyle.normal,
-                                      fontWeight: FontWeight.w300,
-                                      fontSize: 12,
-                                      color: Color(0xFF16FF00))),
-                            ])),
-                    SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+                                fontWeight: FontWeight.w300,
+                                fontSize: 14,
+                                color: Color(0xFF16FF00))
+                            .copyWith(fontFamily: Fonts().light)),
+                    SizedBox(height: 10),
+                    Text(event.eventSite,
+                        style: TextStyle(
+                                fontStyle: FontStyle.normal,
+                                fontWeight: FontWeight.w300,
+                                fontSize: 14,
+                                color: Color(0xFF16FF00))
+                            .copyWith(fontFamily: Fonts().light)),
+                    SizedBox(height: 15),
+                    Text('Участники',
+                        style: TextStyle(
+                                fontStyle: FontStyle.normal,
+                                fontWeight: FontWeight.w300,
+                                fontSize: 18,
+                                color: Color(0xFF333333))
+                            .copyWith(fontFamily: Fonts().medium)),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: <Widget>[
+                          frendList(),
+                          frendList(),
+                          frendList(),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
@@ -140,4 +195,18 @@ class _EventCardState extends State<EventCard> {
           ),
         ),
       );
+
+  Container frendList() => Container(
+      padding: EdgeInsets.only(right: 10),
+      child: Column(children: [
+        InkWell(
+            child: CircleAvatar(
+                backgroundColor: Colors.black,
+                radius: 30,
+                child: CircleAvatar(
+                  radius: 29,
+                  backgroundColor: Colors.red,
+                  backgroundImage: null,
+                )))
+      ]));
 }
