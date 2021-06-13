7cb99b5cc8e5962e14bff17c4af7348a6aa7be1f
Vinogradov Alexander <astronom_35@mail.ru>
2021-06-13 18:13:18 +0300

    wiw

--- frontend/lib/pages/projects/teams.dart
+++ frontend/lib/pages/projects/teams.dart
@@ -104,12 +104,14 @@ class _TeamCardState extends State<TeamCard> {
                       ),
                     ),
                     Text(
-                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing eleit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laborrris nisi ut aliquip ex ea commodo. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                       style: TextStyle(
                         fontFamily: Fonts().regular,
                         fontSize: 14,
                         color: black.withOpacity(0.6),
                       ),
+                      maxLines: 5,
+                      overflow: TextOverflow.ellipsis,
                     ),
                   ],
                 ),
