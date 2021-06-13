533384185a7e209dfd6feac3c61198db45eeac29
Vinogradov Alexander <astronom_35@mail.ru>
2021-06-13 14:16:49 +0300

    Future builder events

--- frontend/lib/pages/projects/events.dart
+++ frontend/lib/pages/projects/events.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:team_up/widgets.dart';
 
 // Проекты - Вкладка События
 class Events extends StatefulWidget {
@@ -9,10 +10,29 @@ class Events extends StatefulWidget {
 }
 
 class _EventsState extends State<Events> {
+  Future<List<int>> eventsIds() async {
+    await Future.delayed(const Duration(seconds: 2));
+    if (DateTime.now().second % 2 == 0) throw Exception("");
+    return [1, 2, 3];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Rofl"),
+    return FutureBuilder<List<int>>(
+      future: eventsIds(),
+      builder: (BuildContext context, AsyncSnapshot<List<int>> snapshot) {
+        if (snapshot.hasError) {
+          return UndergroundNoConnection();
+        } else if (snapshot.hasData) {
+          return Center(
+            child: Text(snapshot.data.toString()),
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
     );
   }
 }
