66092fbdaa70c070e344f4aa21c88c5c6c1562ce
Dragonite24 <qwez-9@mail.ru>
2021-06-14 04:29:34 +0300

    fix success screen

--- frontend/lib/pages/signIn/success.dart
+++ frontend/lib/pages/signIn/success.dart
@@ -23,46 +23,64 @@ class _SuccessState extends State<Success> with SingleTickerProviderStateMixin {
   Widget build(BuildContext context) => WillPopScope(
       onWillPop: () async => false,
       child: UndergroundScaffold(
-          body: Column(
-        children: [
-          Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                SizedBox(height: MediaQuery.of(context).size.height * 0.1),
-                Image.asset('images/logo_imoscow.png', width: 281, height: 58),
-                SizedBox(height: MediaQuery.of(context).size.height * 0.08),
-                Text("Поздравляем!",
+          body: Container(
+        height: MediaQuery.of(context).size.height - 50,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            green.withOpacity(0.2),
+            Colors.white,
+          ],
+        )),
+        child: Column(
+          children: [
+            Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+                  Image.asset('images/logo_imoscow.png',
+                      width: 281, height: 58),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.08),
+                  Text("Поздравляем!",
+                      style: TextStyle(
+                              fontStyle: FontStyle.normal,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 26,
+                              color: Color(0xFF333333))
+                          .copyWith(fontFamily: Fonts().bold)),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+                  Text(
+                    'Ваша учетная запись была успешно создана.\nНажмите "Продолжить", чтобы начать\nиспользовать приложение.',
+                    textAlign: TextAlign.center,
                     style: TextStyle(
                             fontStyle: FontStyle.normal,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 26,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 12,
                             color: Color(0xFF333333))
-                        .copyWith(fontFamily: Fonts().bold)),
-                SizedBox(height: MediaQuery.of(context).size.height * 0.03),
-                Text(
-                  'Ваша учетная запись была успешно создана.\nНажмите "Продолжить", чтобы начать\nиспользовать приложение.',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                          fontStyle: FontStyle.normal,
-                          fontWeight: FontWeight.w500,
-                          fontSize: 12,
-                          color: Color(0xFF333333))
-                      .copyWith(fontFamily: Fonts().light),
-                ),
-                Image.asset(
-                  'images/hands.png',
-                  width: MediaQuery.of(context).size.width,
-                ),
-                UndergroundButton(
-                    text: "Продолжить",
-                    onTap: () {
-                      Navigator.push<void>(
-                          context,
-                          MaterialPageRoute<void>(
-                              builder: (BuildContext context) => MyHomePage()));
-                    }),
-              ]),
-        ],
+                        .copyWith(fontFamily: Fonts().light),
+                  ),
+                  Image.asset(
+                    'images/hands.png',
+                    width: MediaQuery.of(context).size.width,
+                  ),
+                  SizedBox(height: 50),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: UndergroundButton(
+                        text: "Продолжить",
+                        onTap: () {
+                          Navigator.push<void>(
+                              context,
+                              MaterialPageRoute<void>(
+                                  builder: (BuildContext context) =>
+                                      MyHomePage()));
+                        }),
+                  ),
+                ]),
+          ],
+        ),
       )));
 }
